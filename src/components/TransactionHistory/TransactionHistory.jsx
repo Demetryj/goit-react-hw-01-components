1a2ee29827f5db195b09dca3b5f0efa19a821b7c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-10-13 19:25:21 +0300

    add fixed

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 
 export const TransactionHistory = ({ items }) => {
-  console.log(items);
   return (
     <table className="transaction-history">
       <thead>
