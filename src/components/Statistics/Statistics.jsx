4bb6fb8a04b253628c471fb528c722c6070aa1e9
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-10-13 19:25:56 +0300

    add coment

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
-export const Statistics = ({ stats, title }) => {
+export const Statistics = ({ stats, title = null }) => {
   return (
     <section className="statistics">
       {title && <h2 className="title">{title}</h2>}
-
+      {/* если компонент ожидает
+      какое-то значение, а его не передали? - при обращении к свойству объекта
+  props, получим undefined.*/}
       <ul className="stat-list">
         {stats.map(element => {
           return (
