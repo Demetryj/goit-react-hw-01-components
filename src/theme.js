c94c0f278b548ce8ac2f0c08d4a2a37d419c93ff
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-10-13 19:24:46 +0300

    add theme in project

---
+++ src/theme.js
@@ -0,0 +1,47 @@
+export const theme = {
+  colors: {
+    primaryText: '#000',
+    secondaryText: 'rgb(134 131 131)',
+    backgroundMain: '#fff',
+    background: 'rgba(0, 159, 170, 0.493)',
+    accent: 'rgba(37, 40, 239, 0.685)',
+    red: '#f00',
+  },
+
+  spase: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+  fonts: {
+    body: "'Roboto', sans-serif",
+    heading: "'Fira Sans', sans-serif",
+  },
+
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '24px',
+    xl: '32px',
+  },
+
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+};
